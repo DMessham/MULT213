47bee2aca4bc4d202fc6e84dcf1e794e78db4c99
DMessham <20892935+DMessham@users.noreply.github.com>
2025-11-03 11:03:26 -0600

    demo before refactoring code out of index.js

--- demos/nov03/index.js
+++ demos/nov03/index.js
@@ -2,13 +2,22 @@ import { playGame } from './game.js';
 import { User, greetUser } from './user.js';
 
 // Make a user
-var p1 = new User("Alice", "P");
+const p1 = new User("Alice", "P");
+
+const p1moves = {}
 
 // Make another user
-var p2 = new User("Bill", "R");
+const p2 = new User("Bill", "R");
+const p2moves = {}
 
+const stats = {
+    p1wins: 0,
+    p2wins: 0,
+    ties: 0,
+    rounds:0
+}
 // Play the game
-var winner = playGame(p1.move, p2.move);
+let winner = playGame(p1.move, p2.move);
 
 // Print out the result
 if (winner === 0) {
@@ -19,3 +28,24 @@ if (winner === 0) {
     console.log(p2.name + " won!");
 }
 
+//play a few games
+for (let i=0; i<p1.moves.length; i++){
+    console.log(`Round #$(i+1)`);
+
+    const roundWinner = playGame(p1moves[i], p2moves[i])
+
+    if (roundWinner === 0){
+        stats.ties++;
+
+    } else if (roundWinner === 1){
+        stats.p1wins++;
+    } else if (roundWinner === 2){
+        stats.p2wins++;
+    }
+    stats.rounds++
+}
+console.log(`--Game Stats--`);
+console.log(`Games Played: ${stats.rounds}`);
+console.log(`${p1.name} won: ${stats.p1wins} time(s)`);
+console.log(`${p2.name} won: ${stats.p2wins} time(s)`);
+console.log(`${p1.name} & ${p2.name} tied: ${stats.ties} time(s)`);
