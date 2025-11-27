79eff5453dfbd16cc04388a8167d366ed4a2a2c6
DMessham <20892935+DMessham@users.noreply.github.com>
2025-11-27 14:04:39 -0600

    new table generator

--- a1/src/dom.js
+++ a1/src/dom.js
@@ -7,6 +7,44 @@ export function renderImage(el, imgsrc, alttext="") {
     el.innerHTML += `<img src="${imgsrc}">${alttext}</img>`;
   }
 
+export function renderTable(parentEl, tableId, td, headingArray){
+    //heading array is a array for the table leadings
+    //td is a array where each item represents a row, with each row being an array with the first child being the "lable" for the row, and the rest being data that matches the order in the header
+    let table = document.createElement("table");
+    table.setAttribute("id", tableId);
+    try {
+        //make the header
+        let headerNode = document.createElement("tr");
+        for (col=0; col++; col<td[row].length) {
+            let itemNode = document.createElement("th");
+                itemNode.appendChild(headingArray[col]);
+                headerNode.appendChild(itemNode);
+        }
+        table.appendChild(headerNode);
+        //make each row
+        for(row=0; row++; row<td.length){
+            let rowNode = document.createElement("tr");
+            //make each column in each row
+            for (col=0; col++; col<td[row].length) {
+                let itemNode = document.createElement("tr");
+                itemNode.appendChild(headingArray[col]);
+                rowNode.appendChild(itemNode);
+            }
+            table.appendChild(rowNode);
+        }
+        parentEl.appendChild(table)
+    } catch (err) {
+        //create a p element with the 'error' class that has an error message
+        let errNode = document.createElement("p")
+        errNode.className="error"
+        let errorMSG = document.createTextNode(`Table Build Error - ${err.message}`)
+        errNode.appendChild(errorMSG)
+        parentEl.appendChild(errNode)
+        console.log("error in table building", err.message, err)
+    }
+    
+}
+
 export function displayStopList(data){
     let message = "<table><tr><th>Stop name</th><th>location</th><th>latitude</th><th>longitude</th><th>onestop ID</th><th>Actions</th></tr>";
     data.forEach((item) => {
