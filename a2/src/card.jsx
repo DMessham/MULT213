1744b5f8a3304de3881882672c6285fe4b69381d
DMessham <20892935+DMessham@users.noreply.github.com>
2025-12-06 14:42:51 -0600

    work on cards - planning

--- a2/src/card.jsx
+++ a2/src/card.jsx
@@ -1,3 +1,29 @@
 export function Card(props){
-    
+    //feature plans
+        //props.title - 
+        //props.subtitle - (optional) 
+        //props.text - main body text
+        //props.show - feature flags
+            //show.title
+            //show.subtitle
+            //show.image
+            //show.text
+            //show.actions
+        //props.image.url - (optional) the url of an image
+        //props.image.alt - (optional) the alt text for an image, requires props.url
+        //props.actions - 
+    // end docs
+    if (props.show.title){
+
+    }
+    if (props.show.image){
+
+    }
+    //show body text
+
+    //show actions
+    if (props.show.actions){
+
+    }
+
 }
