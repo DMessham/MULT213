3e0b7550296f9b16b9de6712a4886107646a2030
DMessham <20892935+DMessham@users.noreply.github.com>
2025-12-12 14:25:38 -0600

    mui now functional

--- a3/src/App.jsx
+++ a3/src/App.jsx
@@ -1,33 +1,86 @@
 import { useState } from 'react'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider,createTheme } from '@mui/material/styles';
+import {ElevateAppBar} from './topbar'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90ac90',
+    },
+    secondary: {
+      main: '#80b1b7',
+    },
+    divider: '#8d9286',
+    info: {
+      main: '#2a4343',
+      contrastText: 'rgba(48,92,92,0.87)',
+    },
+    error: {
+      main: '#aa2718',
+    },
+    success: {
+      main: '#2e7d32',
+    },
+    warning: {
+      main: '#a6811c',
+      contrastText: 'rgba(74,68,42,0.87)',
+    },
+    background: {
+      default: '#0d120d',
+      paper: '#121c12',
+    },
+  },
+});
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <ThemeProvider theme={theme}>
+    <ElevateAppBar> </ElevateAppBar>
+      
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+            Material UI Vite.js example
+          </Typography>
+          <div>
+          <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </Button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          
+        </Box>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+      </Container>
+      </ThemeProvider>
     </>
   )
 }
---
+++ a3/src/dom.js
@@ -0,0 +1,109 @@
+import { debugmode } from "./transit.js";
+
+//draw text to a html element
+export function renderMessage(el, text) {
+  el.innerHTML = `<p>${text}</p>`;
+}
+
+export function renderImage(el, imgsrc, alttext="") {
+    el.innerHTML += `<img src="${imgsrc}">${alttext}</img>`;
+  }
+
+export function elementFromNode(node, type){
+    let itemNode = document.createElement(type);
+    itemNode.appendChild(node)
+    return itemNode;
+}
+export function renderTable(parentEl, tableId, td, headingArray){
+    if (debugmode){console.log("table gen", parentEl, tableId, td, headingArray)}
+    //heading array is a array for the table leadings
+    //td is a array where each item represents a row, with each row being an array with the first child being the "lable" for the row, and the rest being data that matches the order in the header
+    let table = document.createElement("table");
+    table.setAttribute("id", tableId);
+    try {
+        //make the header
+        let headerNode = document.createElement("tr");
+        for(let col=0; col<headingArray.length; col++) {
+            if (debugmode){console.log("begin table header item", col)}
+            let itemNode = document.createElement("th");
+            itemNode.innerHTML = headingArray[col];
+            headerNode.appendChild(itemNode);
+        }
+        table.appendChild(headerNode);
+        //make each row
+        for (let row=0; row<td.length; row++){
+            let rowNode = document.createElement("tr");
+            //make each item in each row
+            for (let col=0; col<td[row].length; col++) {
+                if (debugmode){console.log("reached table col item gen row,col,value", row, col, td[row][col])};
+                let itemNode = td[row][col];
+                rowNode.appendChild(itemNode);
+            }
+            if (debugmode){console.log(`adding table row`, row)}
+            table.appendChild(rowNode);
+        }
+        if (debugmode){console.log(`adding table to document`, table)}
+        parentEl.appendChild(table)
+        if (debugmode){console.log(`table added`, parentEl)}
+    } catch (err) {
+        //create a p element with the 'error' class that has an error message
+        console.log("error in table building", err.message, err);
+        let errNode = document.createElement("p")
+        errNode.className="error"
+        let errorMSG = document.createTextNode(`Table Build Error - ${err.message}`)
+        errNode.appendChild(errorMSG)
+        parentEl.appendChild(errNode)
+        
+    }
+    
+}
+
+export function displayStopList(data){
+    if (debugmode){console.log(`generating stoplist for`, data)}
+    let message = "<table><tr><th>Stop name</th><th>location</th><th>latitude</th><th>longitude</th><th>onestop ID</th><th>Actions</th></tr>";
+    data.forEach((item) => {
+        if (debugmode){console.log(`generating stoplist for item`, item)}
+        const loc = item.place
+        const location = `${loc.adm1_name}, ${loc.adm0_name}`
+        const point = item.geometry
+        const lat = `${point.coordinates[1]}`
+        const lon = `${point.coordinates[0]}`
+        // build the table row
+        message += `<tr>
+            <td>${item.stop_name}</td>
+            <td>${location}</td>
+            <td>${lat}</td>
+            <td>${lon}</td>
+            <td> ${item.onestop_id}</td>
+            <td><a href="https://maps.google.com/?q=${lat},${lon}">map link</a></td>
+        </tr>`;
+    });
+    message += "</table>";
+    return message
+  }
+
+  export async function displayListButtonEvent(query){
+    if (debugmode){console.log(`begin listbutton for`, query)}
+    const stopOutput = document.querySelector("#stop-output"); 
+    renderMessage(stopOutput, "Loading…");
+    
+    try {
+        if (debugmode){console.log(`generating listbutton for`, query)}
+        const data = await fetchRouteStops(query);
+        if (data.length === 0) {
+            //no data, something has gone wrong
+            renderMessage(stopOutput, `No results found for "${query}".`);
+            return;
+        }
+        
+        let message = `Found ${data.length} result(s) for ${query}`;
+        
+        message += displayStopList(data)
+        
+        renderMessage(stopOutput, message);
+    } catch (err) {
+        renderMessage(stopOutput, `Error: ${err.message}`);
+        console.log(`error in listbutton`, err);
+    }
+};
+
